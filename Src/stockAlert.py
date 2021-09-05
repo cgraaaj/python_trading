cb41da6ab6cf4ecedf47dbf140e1ecfff2abaa81
cgraaaj <cgrvel@gemail.com>
2021-09-05 12:33:11 +0530

    removed unused pakages

--- Src/stockAlert.py
+++ Src/stockAlert.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt  # if you want to plot your findings
-import time
-import smtplib  # enables you to send emails
-from driver import Driver
 import sys
+from driver import Driver
 from telegram_helper import Tele
 
 dri = Driver()
@@ -18,7 +15,7 @@ ticker_btc_monitor = [
 # 13.1
 ticker_equity_monitor = [
     {"ticker": "TRIDENT.NS", "brk_out": 20.00},
-    # {"ticker": "ETH-INR", "brk_out": 138000.00},
+    {"ticker": "ADANIPOWER", "brk_out": 75.00},
     # {"ticker": "BNB-INR", "brk_out": 20400.00},
     # {"ticker": "DOGE-INR", "brk_out": 13.10},
 ]
@@ -27,10 +24,10 @@ ticker_equity_monitor = [
 ticker_to_monitor = ticker_btc_monitor if trade == "bitcoin" else ticker_equity_monitor
 for ticker_dict in ticker_to_monitor:
     ticker_data = dri.get_ticker_data(
-        ticker=ticker_dict['ticker'], range=str(1) + "d", interval="1d"
+        ticker=ticker_dict["ticker"], range=str(1) + "d", interval="1d"
     )
-    if  ticker_data['Close'][0]<= ticker_dict['brk_out']:
+    if ticker_data["Close"][0] <= ticker_dict["brk_out"]:
         fname = "myid"
         message = f'{ticker_dict["ticker"]} has broke {ticker_dict["brk_out"]}'
-        tele = Tele(message,chat_ids_fname=fname)
+        tele = Tele(message, chat_ids_fname=fname)
         tele.send_message("-----Breakout Alert-----")
